927a37378742c2a65ee7c6e2eb91499da409a754
Rodrigo Ordoñez <rodrigoordonez@nube.unadmexico.mx>
2024-09-23 19:56:58 -0500

    Despliegue en GitHub Pages

--- Components/EditorPanel.js
+++
@@ -1,6 +0,0 @@
-// /components/EditorPanel.js
-const EditorPanel = () => {
-  return <div>Panel de Editor</div>;
-};
-
-export default EditorPanel;
--- pages/login/layout.js
+++
@@ -1,9 +0,0 @@
-export default function LoginLayout({ children }) {
-  return (
-    <div className="login-layout">
-      <header>Inicia Sesión en Nuestro Sistema</header>
-      <main>{children}</main>
-      <footer>Todos los derechos reservados 2024</footer>
-    </div>
-  );
-}
