458499bf01cacf2bc012da33b58c28d852a082ad
Rodrigo Ordoñez <rodrigoordonez@nube.unadmexico.mx>
2024-09-25 23:13:20 -0500

    Fix NavBar and update styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
 import "animate.css/animate.min.css";
 import "../styles/globals.css";
-import "animate.css/animate.min.css";
-import "font-awesome/css/font-awesome.min.css"; // Asegúrate de que esta línea esté presente
+import "font-awesome/css/font-awesome.min.css";
 import { NextUIProvider } from "@nextui-org/react";
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Novecento+Wide&family=Raleway:wght@400&display=swap"
-        rel="stylesheet"
-      />
-      <Component {...pageProps} />
-    </>
+    <NextUIProvider>
+      <>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Novecento+Wide&family=Raleway:wght@400&display=swap"
+          rel="stylesheet"
+        />
+        <Component {...pageProps} />
+      </>
+    </NextUIProvider>
   );
 }
 
