23cec8f5c4b8e7780f2a671caf087e03654b2ca0
Rodrigo Ordoñez <rodrigoordonez@nube.unadmexico.mx>
2024-09-26 10:09:55 -0500

    Eliminar archivos no deseados del seguimiento

--- OneDrive - Universidad Abierta y a Distancia de México/Unadm/3er bloque 2024/Proyecto terminal I – DPT1/archivo-historico/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // Añadimos autoprefixer
-  },
-};
-
-export default config;
